2fcd007e418ea7f3a56dcc1ac7537aeb51430e69
Hiperonii <hiperony@gmail.com>
2023-06-14 15:34:49 +0700

    Update integration test for passcode lock

--- integration_test/smoke_test.dart
+++ integration_test/smoke_test.dart
@@ -9,8 +9,47 @@ void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
 
-  group('Smoke test', () {
-    testWidgets('Test task list screen should be able to scroll and switch tab', (tester) async {
+  group('TaskList test', () {
+    testWidgets('''1. Task list should be able to scroll 
+                   2. Task list able to switch tab''', (tester) async {
+      app.main();
+      // Wait for UI settle, expect to see passcode screen
+      await tester.pumpAndSettle();
+      expect(_findByKey('Keyboard'), findsOneWidget);
+
+      // Input correct passcode, expect to navigate to main screen
+      await _tapVirtualKeyboard(tester, KeySymbols.one);
+      await _tapVirtualKeyboard(tester, KeySymbols.two);
+      await _tapVirtualKeyboard(tester, KeySymbols.three);
+      await _tapVirtualKeyboard(tester, KeySymbols.four);
+      await _tapVirtualKeyboard(tester, KeySymbols.five);
+      await _tapVirtualKeyboard(tester, KeySymbols.six);
+
+      // Wait for UI settle, expect to see to-do tab
+      await tester.pumpAndSettle();
+      expect(_findByKey('TODO'), findsOneWidget);
+
+      // Wait for API load, expect to see first item
+      await _waitForAction(tester);
+      expect(_findByText('Read a book'), findsOneWidget);
+
+      // Loop scroll to bottom of the list, expect list able to scroll by seeing last item of the list
+      for (int i = 0; i < 8; i++) {
+        await tester.drag(find.byType(ListView), const Offset(0, -400));
+        await _waitForAction(tester);
+      }
+      expect(_findByText('Buy groceries'), findsOneWidget);
+
+      // Tap `Doing` tab, expect to see item in the doing list
+      await _tapByKey(tester, 'DOING');
+      await _waitForAction(tester);
+      expect(_findByText('The best way to predict the future is to invent it.'), findsOneWidget);
+    });
+  });
+
+  group('Passcode test', () {
+    testWidgets('''1. Passcode should be navigate to main screen only if code is correct 
+                   2. Lock on inactive duration''', (tester) async {
       app.main();
       // Wait for UI settle, expect to see passcode screen
       await tester.pumpAndSettle();
@@ -30,6 +69,7 @@ void main() {
       await _waitForAction(tester, const Duration(seconds: 1));
       expect(_findByText('Incorrect passcode'), findsNothing);
 
+      // Input correct passcode, expect to navigate to main screen
       await _tapVirtualKeyboard(tester, KeySymbols.one);
       await _tapVirtualKeyboard(tester, KeySymbols.two);
       await _tapVirtualKeyboard(tester, KeySymbols.three);
@@ -41,21 +81,14 @@ void main() {
       await tester.pumpAndSettle();
       expect(_findByKey('TODO'), findsOneWidget);
 
-      // Wait for API load, expect to see first item
-      await _waitForAction(tester);
-      expect(_findByText('Read a book'), findsOneWidget);
+      // Wait for 9 seconds then active the screen, expect to stay on main screen
+      await _waitForAction(tester, const Duration(seconds: 8));
+      await _tapByKey(tester, 'DONE');
+      expect(_findByKey('TODO'), findsOneWidget);
 
-      // Loop scroll to bottom of the list, expect to see last item
-      for (int i = 0; i < 8; i++) {
-        await tester.drag(find.byType(ListView), const Offset(0, -400));
-        await _waitForAction(tester);
-      }
-      expect(_findByText('Buy groceries'), findsOneWidget);
-
-      // Tap `Doing` tab, expect to see item in the doing list
-      await _tapByKey(tester, 'DOING');
-      await _waitForAction(tester);
-      expect(_findByText('The best way to predict the future is to invent it.'), findsOneWidget);
+      // Wait for 10 seconds, expect to see passcode screen
+      await _waitForAction(tester, const Duration(seconds: 10));
+      expect(_findByKey('Keyboard'), findsOneWidget);
     });
   });
 }
